aa8046bd5980f7a91d092d12e73ea4eb253b151d
Ernest <ooquirrino@gmail.com>
2024-09-26 21:40:00 +0300

    Week02 In Week

---
+++ week02/scripts/bom.js
@@ -0,0 +1,24 @@
+const input = document.querySelector("#favchap");
+const button = document.querySelector('button');
+const list = document.querySelector('#list')
+const li = document.createElement('li');
+const deleteButton = document.createElement('button');
+
+li.textContent = input.value
+deleteButton.textContent = 'X'
+li.append(deleteButton)
+list.append(li)
+
+button.addEventListener('click', function () {
+    if (input.value.trim() != '') {
+        const currentYear = new Date().getFullYear();
+        footerParagraphs[1].innerText = `Last modified: ${document.lastModified}`;
+    }
+});
+
+deleteButton.addEventListener('click', function () {
+    list.removeChild(li);
+    input.focus();
+});
+input.value = ''
+input.focus();
